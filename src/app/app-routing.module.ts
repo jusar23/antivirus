a8da18235a865c335e44bd93f7b16f3651ff20dc
B4ST1N2 <svanegas899@soyudemedellin.edu.co>
2024-08-19 20:14:24 -0500

    Routing-Terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogingComponent } from './loging/loging.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { HomeComponent } from './home/home.component';
+import { OportunidadesComponent } from './oportunidades/oportunidades.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login' , component: LogingComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'inicio', component: HomeComponent},
+  {path: 'oportunidades', component: OportunidadesComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
